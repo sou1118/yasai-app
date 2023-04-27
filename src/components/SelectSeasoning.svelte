50091c11632c70da118a089b63f79f4c68939e77
sou1118 <hello@kattyan.dev>
2023-04-27 22:05:49 +0900

    :sparkles: add components

---
+++ src/components/SelectSeasoning.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { seasonings } from '../data';
+    export let selectedSeasoning;
+  </script>
+  
+  <select bind:value={selectedSeasoning} class="select select-bordered">
+    <option value="">調味料を選択</option>
+    {#each seasonings as seasoning}
+      <option value={seasoning.id}>{seasoning.name}</option>
+    {/each}
+  </select>
---
+++ src/components/SelectVegetable.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { vegetables } from '../data';
+    export let selectedVegetable;
+  </script>
+  
+  <select bind:value={selectedVegetable} class="select select-bordered">
+    <option value="">野菜を選択</option>
+    {#each vegetables as vegetable}
+      <option value={vegetable.id}>{vegetable.name}</option>
+    {/each}
+  </select>
