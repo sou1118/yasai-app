251eabfb6707c295432e629ea4666f525c7e6756
sou1118 <hello@kattyan.dev>
2023-04-27 22:16:53 +0900

    :sparkles: update +pdata.ts & install adapter-static

--- src/data.ts
+++ src/data.ts
@@ -1,11 +1,19 @@
 export const vegetables = [
     { id: 1, name: 'きゅうり' },
+    { id: 2, name: 'トマト' },
   ];
   
   export const seasonings = [
     { id: 1, name: '塩' },
+    { id: 2, name: '砂糖' },
+    { id: 3, name: '醤油' },
   ];
   
   export const storageTimes = [
     { vegetableId: 1, seasoningId: 1, duration: 7 },
+    { vegetableId: 1, seasoningId: 2, duration: 3 },
+    { vegetableId: 1, seasoningId: 3, duration: 14 },
+    { vegetableId: 2, seasoningId: 1, duration: 7 },
+    { vegetableId: 2, seasoningId: 2, duration: 3 },
+    { vegetableId: 2, seasoningId: 3, duration: 14 },
   ];
