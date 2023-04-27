d0d14178fe13cd66d3a4690efa89298d97b0c3d9
sou1118 <hello@kattyan.dev>
2023-04-27 22:03:32 +0900

    :sparkles: add data.ts

---
+++ src/data.ts
@@ -0,0 +1,11 @@
+export const vegetables = [
+    { id: 1, name: 'きゅうり' },
+  ];
+  
+  export const seasonings = [
+    { id: 1, name: '塩' },
+  ];
+  
+  export const storageTimes = [
+    { vegetableId: 1, seasoningId: 1, duration: 7 },
+  ];
